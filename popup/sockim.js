7f269853ae31917e92cd57975184e5b73fea767b
BetterLordWilliam <willjordan05@icloud.com>
2025-11-08 00:30:47 -0800

    WO: Correct cookie permissions and security policies in order that the application can be embedded from the extension.

---
+++ popup/sockim.js
@@ -0,0 +1,12 @@
+function onButtonClick(e) {
+    e.preventDefault();
+    console.log("We are gaming now");
+}
+
+// let iframe = document.getElementById("frame");
+
+// window.addEventListener('resize', () => {
+//     console.log('life is changing');
+//     iframe.style.width = window.innerWidth + 'px';
+//     iframe.style.height = window.innerHeight + 'px';
+// })
